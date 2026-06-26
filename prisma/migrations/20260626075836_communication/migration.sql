b44ff3877d1ba020a90809dc4eea5cf2f9a1938f
anupama-rumsan <anupama@rumsan.net>
2026-06-26 16:17:55 +0545

    migration updated

--- prisma/migrations/20260626075836_communication/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `reaponseDetails` on the `tbl_communication` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "tbl_communication" DROP COLUMN "reaponseDetails",
-ADD COLUMN     "responseDetails" JSONB;
